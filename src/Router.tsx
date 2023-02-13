9fbe9c99b7f8a6f90f7ea1d5c03735b559ddf5a3
Lucas Roseti <lc.roseti@gmail.com>
2023-02-12 21:23:26 -0300

    Fix pages import
    Fix pages import in Blog and Post routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,8 +2,8 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { DefaultLayout } from './layouts/DefaultLayout'
 
-import { Blog } from './page/Blog'
-import { Post } from './page/Post'
+import { Blog } from './pages/Blog'
+import { Post } from './pages/Post'
 
 export function Router() {
   return (
